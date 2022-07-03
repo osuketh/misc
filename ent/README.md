61e2ccc5d8eb459be3eee2b6d09d9b3af6bb41ea
osuketh <dish230@gmail.com>
2022-07-03 21:09:00 +0900

    fix

--- ent/README.md
+++ ent/README.md
@@ -1,28 +1,22 @@
 # misc-ent
 
+Migration files using ent's versioned migration is formatted for https://github.com/golang-migrate/migrate as default.
+
 ```bash
-# 1. gen graphql schema
+# gen graphql schema
 gqlgen
 
 # versioned migration (may not use)
 go run ./cmd/migration
 
-# 2. migration
-dbmate up
+migrate -database 'mysql://root:password@tcp(localhost:3306)/misc' -path migrations up
 
-# 3. gen schema (https://github.com/ariga/entimport)
-go run ariga.io/entimport/cmd/entimport -dsn "mysql://root:password@tcp(localhost:3306)/misc"
+# gen schema (https://github.com/ariga/entimport)
+# go run ariga.io/entimport/cmd/entimport -dsn "mysql://root:password@tcp(localhost:3306)/misc"
 
-# 4. gen ent schema
+# gen ent schema
 go generate ./ent/...
 
-# 5. run server
+# run server
 go run ./cmd/api-gql
-```
-
-Migration files using ent's versioned migration is formatted for https://github.com/golang-migrate/migrate as default.
-
-
-```
-migrate -database 'mysql://root:password@tcp(localhost:3306)/misc' -path migrations up
 ```
--- ent/migrations/20220703094903_changes.up.sql
+++
@@ -1,8 +0,0 @@
--- modify "schema_migrations" table
-ALTER TABLE `schema_migrations` COLLATE utf8mb4_bin, MODIFY COLUMN `version` varchar(255) NOT NULL;
--- create "todos" table
-CREATE TABLE `todos` (`id` bigint NOT NULL AUTO_INCREMENT, `text` varchar(255) NOT NULL, `created_at` timestamp NULL, `status` enum('IN_PROGRESS','COMPLETED') NOT NULL, `priority` bigint NOT NULL, `todo_parent` bigint NULL, PRIMARY KEY (`id`), CONSTRAINT `todos_todos_child_todos` FOREIGN KEY (`todo_parent`) REFERENCES `todos` (`id`) ON DELETE SET NULL) CHARSET utf8mb4 COLLATE utf8mb4_bin;
--- create "users" table
-CREATE TABLE `users` (`id` varchar(36) NOT NULL, `name` varchar(255) NOT NULL, `email` varchar(255) NOT NULL, `role` enum('admin','user') NOT NULL, `deleted_at` timestamp NULL, `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`id`), UNIQUE INDEX `email` (`email`), INDEX `user_name` (`name`)) CHARSET utf8mb4 COLLATE utf8mb4_bin ENGINE = INNODB;
--- create "groups" table
-CREATE TABLE `groups` (`id` varchar(255) NOT NULL, `name` varchar(255) NOT NULL, `user_id` varchar(36) NULL, PRIMARY KEY (`id`), CONSTRAINT `groups_users_groups` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL) CHARSET utf8mb4 COLLATE utf8mb4_bin;
